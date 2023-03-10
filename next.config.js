4d4eb10fe11fe8b341c0f7a657d36904b7d34a01
muffafa <mm.savar@gmail.com>
2023-03-10 20:28:56 +0300

    cors resolved

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
-/*const nextConfig = {
+const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig*/
+module.exports = nextConfig
 
-module.exports = () => {
-  const rewrites = () => {
-    return [
-      {
-        source: "/randomQuote",
-        destination: "https://programming-quotesapi.vercel.app/api/random",
-      },
-    ];
-  };
-  return {
-    rewrites,
-  };
-};
+// module.exports = () => {
+//   const rewrites = () => {
+//     return [
+//       {
+//         source: "/randomQuote",
+//         destination: "https://programming-quotesapi.vercel.app/api/random",
+//       },
+//     ];
+//   };
+//   return {
+//     rewrites,
+//   };
+// };
