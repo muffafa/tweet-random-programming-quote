697abb253a1ca4f11bdd12b9f8c774e7d55d0a0d
muffafa <mm.savar@gmail.com>
2023-03-10 06:31:56 +0300

    old verison

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
 
   const fetchRandomQuote = async () => {
     const res = await axios.get(
-      "https://tweet-random-programming-quote.vercel.app/https://programming-quotesapi.vercel.app/api/random"
+      "https://programming-quotesapi.vercel.app/api/random"
     );
     return {
       author: res.data.author,
