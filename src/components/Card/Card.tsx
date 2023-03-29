2c3dfe2d1cf6c21f268f0342f7c5e0a813399ace
muffafa <mm.savar@gmail.com>
2023-03-29 16:21:54 +0300

    project completed

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,79 +1,56 @@
 import React from "react";
-import { useState, useEffect, use } from "react";
-import axios, { AxiosError } from "axios";
 import Icon from "@mdi/react";
 import { mdiFormatQuoteOpen } from "@mdi/js";
+import { selectQuote, setQuoteStatus, setQuoteSlice } from "@/redux/QuotesSlice";
+import { useSelector } from "react-redux";
 
-interface Props {
-  children?: React.ReactNode;
-}
+const Content = () => {
+  const quoteState = useSelector(selectQuote);
 
-type Quote = {
-  author: string;
-  quote: string;
-};
+  let content;
 
-const Card: React.FC<Props> = ({ children }) => {
-  const [quote, setQuote] = useState<Quote | null>();
-  const [state, setState] = useState<"loading" | "error" | "done" | null>(null);
-
-  const fetchRandomQuote = async () => {
-    const res = await axios.get(
-      "https://programming-quotesapi.vercel.app/api/random"
+  if (quoteState.status === "idle") {
+    content = <p>Welcome</p>;
+  } else if (quoteState.status === "pending") {
+    content = <p>Loading...</p>;
+  } else if (quoteState.status === "error") {
+    content = <p>Error</p>;
+  } else if (quoteState.status === "done") {
+    content = (
+      <div className="text-sm md:text-base flex flex-col justify-between h-full space-y-24">
+        <p>{quoteState?.quote}</p>
+        <p className="text-end">{quoteState?.author}</p>
+      </div>
     );
-    return {
-      author: res.data.author,
-      quote: res.data.quote,
-    };
-  };
-
-  const getNewTweet = () => {
-    console.log("getNewTweet");
-    setState("loading");
-    fetchRandomQuote()
-      .then((quote) => setQuote(quote))
-      .then(() => setState("done"))
-      .catch((e: Error | AxiosError) => setState("error"));
-  };
-
-  useEffect(() => {
-    getNewTweet();
-  }, []);
-
-  const shareTweet = () => {
-    const tweetText = quote?.quote + " - " + quote?.author;
-    console.log("shareTweet");
-    const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
-      tweetText
-    )}`;
-    window.open(tweetUrl, "_blank");
-  };
+  }
 
   return (
-    <div className="flex w-full justify-center my-10">
+    <>
+      <div className="flex items-center">
+        <Icon
+          path={mdiFormatQuoteOpen}
+          size={"6rem"}
+          color={"#14171a"}
+          className=""
+        />
+        <div>
+          <h1 className="text-base md:text-xl">
+            Tweet Random Programmer Quote
+          </h1>
+          <h2 className="text-sm md:text-lg">@Quote Programmer</h2>
+        </div>
+      </div>
+      <div className="p-5 h-full">{content}</div>
+    </>
+  );
+};
+
+const Card = () => {
+  return (
+    <div className="flex w-full justify-center text-white">
       <div className="flex sm:aspect-square w-128 p-5 bg-twitter-900">
         <div className="bg-gradient-to-b from-twitter-500 to-twitter-90 rounded-xl p-6 w-full">
-          <div className="container flex flex-col h-full">
-            <div className="flex items-center">
-              <Icon path={mdiFormatQuoteOpen} size={"6rem"} color={"#14171a"} className=""/>
-              <div>
-                <h1 className="text-base md:text-xl">Tweet Random Programmer Quote</h1>
-                <h2 className="text-sm md:text-lg">@Quote Programmer</h2>
-              </div>
-            </div>
-            <div className="p-5 h-full">
-              {state === "loading" ? (
-                <p>loading...</p>
-              ) : state === "error" ? (
-                <p>error...</p>
-              ) : (
-                <div className="text-sm md:text-base flex flex-col justify-between h-full space-y-24">
-                  <p>{quote?.quote}</p>
-                  <p className="text-end">{quote?.author}</p>
-                </div>
-              )}
-            </div>
-          </div>
+          <div className="container flex flex-col h-full">{Content()}</div>
         </div>
       </div>
     </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux';
+import { wrapper } from "../redux/store";
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { store, props } = wrapper.useWrappedStore(pageProps);
+  return(
+    <Provider store={store}>
+      <Component {...props.pageProps} />
+    </Provider>
+  )
 }
